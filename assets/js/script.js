de9c9188a542583667ad31127d18948d8656b149
Hen0678 Cohen <andycohen32@gmail.com>
2024-05-12 14:18:29 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+co  nst choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+// restart //
+const restart = document.getElementById('restart');
+const modal = querySelector('.modal');
+// keeping score
+const scoreboard = {
+    player: 0,
+    computer: 0
+};
+
+// Play game
+function play(e) {
+    console.log(e.target.id);
+}
+
+// Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
